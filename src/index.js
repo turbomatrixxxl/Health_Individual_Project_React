8f3e0bd4a271bd051dfe0917556632a8329fe988
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-11-29 17:31:27 +0200

    final commit !!! all works fine !!! :)))))))

--- src/index.js
+++ src/index.js
@@ -33,31 +33,3 @@ root.render(
 
 reportWebVitals();
 
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-// import "./styles/variables.css";
-// import { Provider } from "react-redux";
-// import { store } from "./redux/store";
-// import { HashRouter } from "react-router-dom";
-
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <HashRouter
-//         future={{
-//           v7_startTransition: true,
-//           v7_relativeSplatPath: true,
-//         }}
-//       >
-//         <App />
-//       </HashRouter>
-//     </Provider>
-//   </React.StrictMode>
-// );
-
-// reportWebVitals();
