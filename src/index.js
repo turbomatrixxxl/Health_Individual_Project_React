7ef76b118dde9ff51d44fadedd099c43bd2c96f6
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-11-29 17:25:13 +0200

    changed router again...

--- src/index.js
+++ src/index.js
@@ -1,38 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-// import "./styles/variables.css";
-// import { Provider } from "react-redux";
-// import { store } from "./redux/store";
-// import { BrowserRouter } from "react-router-dom";
-
-// // Use the correct basename based on the environment
-// const basename =
-//   process.env.NODE_ENV === "production"
-//     ? "/Health_Individual_Project_React"
-//     : "/";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <BrowserRouter
-//         future={{
-//           v7_startTransition: true,
-//           v7_relativeSplatPath: true,
-//         }}
-//         basename={basename} // Dynamically set the basename
-//       >
-//         <App />
-//       </BrowserRouter>
-//     </Provider>
-//   </React.StrictMode>
-// );
-
-// reportWebVitals();
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -41,22 +6,58 @@ import reportWebVitals from "./reportWebVitals";
 import "./styles/variables.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+// Use the correct basename based on the environment
+const basename =
+  process.env.NODE_ENV === "production"
+    ? "/Health_Individual_Project_React"
+    : "/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter
+      <BrowserRouter
         future={{
           v7_startTransition: true,
           v7_relativeSplatPath: true,
         }}
+        basename={basename} // Dynamically set the basename
       >
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
 
 reportWebVitals();
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+// import "./styles/variables.css";
+// import { Provider } from "react-redux";
+// import { store } from "./redux/store";
+// import { HashRouter } from "react-router-dom";
+
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <HashRouter
+//         future={{
+//           v7_startTransition: true,
+//           v7_relativeSplatPath: true,
+//         }}
+//       >
+//         <App />
+//       </HashRouter>
+//     </Provider>
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
